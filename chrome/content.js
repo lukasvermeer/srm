8d48b63d2a628f0a2e6a1342478dc92ce8e9f344
Lukas Vermeer <lukas@lukasvermeer.nl>
2019-10-17 10:17:17 +0200

    Add support for Google Optimize MVP
    This is a first step towards #1. Not too happy with the implementation,
    but it works, and will probably need more changes to the overall
    architecture when implementing support for other platforms anyway. This
    is good enough for now, as it allows us to start focusing on the UX, as
    well as testing some edge cases.

--- chrome/content.js
+++ chrome/content.js
@@ -1,15 +1,43 @@
-document.querySelectorAll('input').forEach(i => i.addEventListener("input", function() {
-  var a = parseInt(document.getElementById("atraffic").value);
-  var b = parseInt(document.getElementById("btraffic").value);
-  var e = parseFloat(document.getElementById("expectedprop").value);
+switch(window.location.origin) { // TODO switch is bad; find alternative
+  case "https://lukasvermeer.github.io":
+    document.querySelectorAll('input').forEach(i => i.addEventListener("input", function() {
+      var a = parseInt(document.getElementById("atraffic").value);
+      var b = parseInt(document.getElementById("btraffic").value);
+      var e = parseFloat(document.getElementById("expectedprop").value);
 
+      checkSRM(a, b, e);
+    }, false));
+    break;
+  case "https://optimize.google.com":
+    setInterval(function() { // TODO find way to detect document changes instead of using polling
+      var d = document.getElementsByClassName("opt-variant-sessions-subtitle");
+      if (d.length > 1) {
+        var a = parseInt(d[0].innerText.split(" ")[0]);
+        var b = parseInt(d[1].innerText.split(" ")[0]);
+        var e = 0.5; // TODO extract intended split from GO
+
+        checkSRM(a, b, e);
+      }
+    }, 1000);
+    break;
+}
+
+function checkSRM(a, b, e) {
   var n = a + b;
   var p = b / n;
   var r = jStat.ztest(p, e, Math.sqrt(p*(1-p)/n));
 
+  var c = "white";
   if (r < 0.0001) {
-    document.body.style.backgroundColor = "red"
-  } else {
-    document.body.style.backgroundColor = "white"
+    c = "red"
   }
-}, false));
+
+  switch(window.location.origin) {
+    case "https://lukasvermeer.github.io":
+      document.body.style.backgroundColor = c;
+      break;
+    case "https://optimize.google.com":
+      document.querySelectorAll(".opt-variant-sessions-subtitle").forEach(i => i.style.backgroundColor = c)
+      break;
+  }
+};
