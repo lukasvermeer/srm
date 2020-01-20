cabe6ea6d258f5fbbdec93ceb341cf762670e1b9
Lukas Vermeer <lukas@lukasvermeer.nl>
2020-01-20 21:15:17 +0100

    Almost complete rewrite. Some regressions, but hopefully more future-proof.

--- chrome-extension/src/content.js
+++ chrome-extension/src/content.js
@@ -1,118 +1,142 @@
-switch (window.location.origin) { // TODO switch is bad; find alternative
-  case "https://lukasvermeer.github.io":
-    document.querySelectorAll('input').forEach(i => i.addEventListener("input", function() {
-      var a = parseInt(document.getElementById("atraffic").value);
-      var b = parseInt(document.getElementById("btraffic").value);
-      var e = parseFloat(document.getElementById("expectedprop").value);
+// Sample Ratio Mismatch (SRM) Checker
+//
+// This Chrome Extension automatically performs SRM checks and flags potential
+// data quality issues on supported experimentation platforms.
+//
+// More information at https://lukasvermeer.nl/srm/
 
-      checkSRM(a, b, e);
-    }, false));
-    break;
-  case "https://optimize.google.com":
-    // Load the Details Tab in the background to get expected proportion of variants.
-    function new_iframe() {
-      if (location.href.slice(-6) == "report") {
-        var old_iframe = document.getElementById('iframeforweight');
-        if (old_iframe != null) {
-          old_iframe.parentNode.removeChild(old_iframe);
-        }
+// Define (default) parameters.
+const params = {
+  pValueThreshold: 0.0001,
+};
 
-        var ifrm = document.createElement("iframe");
-        ifrm.id = "iframeforweight"
-        ifrm.src = location.href.slice(0,-7);
-        ifrm.style.display = "none";
-        document.body.appendChild(ifrm);
-      }
-    }
-    new_iframe();
-
-    // Listen for changing URL to reload iframe for proportions
-    chrome.runtime.onMessage.addListener(
-      function (request, sender, sendResponse) {
-        if (request.message === 'URL has changed') {
-          new_iframe();
-          srm_checked = false;
-        }
-      });
-
-    var srm_checked = false; // TODO: Listen for changes to do check when content loads.
-    setInterval(function () {
-      if (!srm_checked) {
-        // Get sample counts
-        var d = document.querySelectorAll('opt-multi-objective .opt-variant-sessions-subtitle');
-        var iframeforweight = document.getElementById('iframeforweight');
-        var weightnodes = iframeforweight.contentWindow.document.getElementsByClassName("opt-variation-weight");
-
-        if (d.length > 1 && weightnodes.length > 1) {
-          // Get unrounded custom proportions (if any)
-          // TODO: Must be a better way to do this.
-          (iframeforweight.contentWindow.document.getElementsByClassName('opt-variation-weight')[0]).click();
-          var weightnodes_custom = iframeforweight.contentWindow.document.querySelectorAll('.opt-edit-weights-list input');
-
-          if (weightnodes.length > 1) {
-            sessioncounts = [];
-            weights = [];
-
-            // Fill the arrays
-            for (var i = 0; i < d.length; i++) {
-              var sessions = parseInt(d[i].innerText.replace(/,/g, '').split(" ")[0]);
-              var weightnode = weightnodes[i].getElementsByClassName("ng-binding");
-              var weight = parseInt(weightnode[0].innerHTML);
-              // Use custom unrounded weights when available.
-              // TODO: Must be a better way to do this too.
-              if (weightnodes_custom.length > 1) {
-                weight = parseFloat(weightnodes_custom[i].value);
-              }
-              sessioncounts.push(sessions);
-              weights.push(weight);
-            }
-
-            // Check all combinations for SRMs
-            var j = 0;
-            var k = 1;
-            var checksperformed = 0;
-            while (j < sessioncounts.length) {
-              while (k < sessioncounts.length) {
-                var l = weights[j] / (weights[j] + weights[k]);
-                console.log('SRM checked between: ' + sessioncounts[j] + ' and ' + sessioncounts[k] + ' with expected weight ' + l);
-                checkSRM(sessioncounts[j], sessioncounts[k], l, j, k, checksperformed);
-                checksperformed++;
-                k++;
-              }
-              j++;
-              var k = j + 1;
-            }
-            srm_checked = true;
-          }
-        }
-      }
-    }, 1000);
-}
+// Generic (non-platform specific) functions
+const platform = window.location.host;
 
 function checkSRM(a, b, e, identa, identb, checksperformed) {
-  var n = a + b;
-  var p = b / n;
-  var r = jStat.ztest(p, e, Math.sqrt(p*(1-p)/n));
+  const n = a + b;
+  const p = b / n;
+  const r = jStat.ztest(p, e, Math.sqrt(p*(1-p)/n));
 
-  backgrounds = ['#FFA07A', '#FA8072', '#CD5C5C', '#DC143C', '#B22222', '#FF0000', '#800000', '#FF4500', '#DB7093']
-
-  switch(window.location.origin) {
-    case "https://lukasvermeer.github.io":
-      document.body.style.backgroundColor = r < 0.0001 ? "red" : "white";
-      break;
-    case "https://optimize.google.com":
-      if (r < 0.0001) {
-        checksperformed++;
-        function createidentspan(identifier) {
-          var identspan = document.createElement("span");
-          identspan.style.cssText = "background-color: " + backgrounds[checksperformed] + "; padding: 1px 2.5px; margin-right: 1px; color: white; border-radius: 3px;";
-          var identtext = document.createTextNode("SRM#" + checksperformed);
-          identspan.appendChild(identtext);
-          document.querySelectorAll('opt-multi-objective .opt-variant-sessions-subtitle')[identifier].appendChild(identspan);
-        }
-        createidentspan(identa);
-        createidentspan(identb);
-      }
-      break;
+  if (r < params.pValueThreshold) {
+    platforms[platform].flagSRM();
+  } else {
+    platforms[platform].unflagSRM();
   }
+}
+
+// Define object to contain platform specific methods.
+const platforms = {
+
+  // SRM Checker Chrome Extension microsite
+  // TODO Remove this once we support more than one real platform.
+  'lukasvermeer.nl': {
+    init() {
+      document.querySelectorAll('input').forEach(i => i.addEventListener('input', () => {
+        const a = parseInt(document.getElementById('atraffic').value, 10);
+        const b = parseInt(document.getElementById('btraffic').value, 10);
+        const e = parseFloat(document.getElementById('expectedprop').value, 10);
+
+        checkSRM(a, b, e);
+      }, false));
+    },
+    flagSRM() {
+      document.body.style.backgroundColor = 'red';
+    },
+    unflagSRM() {
+      document.body.style.backgroundColor = 'white';
+    },
+  },
+
+  // Google Optimize
+  'optimize.google.com': {
+    init() {
+      // Load the Details Tab in the background to get expected proportion of variants.
+      function newIframe() {
+        if (location.href.slice(-6) == 'report') {
+          const oldIframe = document.getElementById('iframeforweight');
+          if (oldIframe != null) {
+            oldIframe.parentNode.removeChild(oldIframe);
+          }
+
+          const ifrm = document.createElement('iframe');
+          ifrm.id = 'iframeforweight';
+          ifrm.src = location.href.slice(0,-7);
+          ifrm.style.display = 'none';
+          document.body.appendChild(ifrm);
+        }
+      }
+      newIframe();
+
+      // Listen for changing URL to reload iframe for proportions
+      chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+          if (request.message === 'URL has changed') {
+            newIframe();
+            srmChecked = false;
+          }
+        },
+      );
+
+      let srmChecked = false; // TODO: Listen for changes to do check when content loads.
+      setInterval(function () {
+        if (!srmChecked) {
+          // Get sample counts
+          const d = document.querySelectorAll('opt-multi-objective .opt-variant-sessions-subtitle');
+          const iframeforweight = document.getElementById('iframeforweight');
+          const weightnodes = iframeforweight.contentWindow.document.getElementsByClassName('opt-variation-weight');
+
+          if (d.length > 1 && weightnodes.length > 1) {
+            // Get unrounded custom proportions (if any)
+            // TODO: Must be a better way to do this.
+            (iframeforweight.contentWindow.document.getElementsByClassName('opt-variation-weight')[0]).click();
+            const weightnodesCustom = iframeforweight.contentWindow.document.querySelectorAll('.opt-edit-weights-list input');
+
+            if (weightnodes.length > 1) {
+              const sessioncounts = [];
+              const weights = [];
+
+              // Fill the arrays
+              for (let i = 0; i < d.length; i += 1) {
+                const sessions = parseInt(d[i].innerText.replace(/,/g, '').split(' ')[0], 10);
+                const weightnode = weightnodes[i].getElementsByClassName('ng-binding');
+                let weight = parseInt(weightnode[0].innerHTML, 10);
+                // Use custom unrounded weights when available.
+                // TODO: Must be a better way to do this too.
+                if (weightnodesCustom.length > 1) {
+                  weight = parseFloat(weightnodesCustom[i].value, 10);
+                }
+                sessioncounts.push(sessions);
+                weights.push(weight);
+              }
+
+              // Check all combinations for SRMs
+              let j = 0;
+              let k = 1;
+              let checksperformed = 0;
+              while (j < sessioncounts.length) {
+                while (k < sessioncounts.length) {
+                  const l = weights[j] / (weights[j] + weights[k]);
+                  checkSRM(sessioncounts[j], sessioncounts[k], l, j, k, checksperformed);
+                  checksperformed += 1;
+                  k += 1;
+                }
+                j += 1;
+                k = j + 1;
+              }
+              srmChecked = true;
+            }
+          }
+        }
+      }, 1000);
+    },
+    flagSRM() {
+      document.querySelectorAll('.opt-variant-sessions-subtitle').forEach(i => i.style.cssText = 'background-color: red; color: white; padding: 1px 3px; border-radius: 3px;');
+    },
+    unflagSRM() {
+      // TODO remove SRM warning if needed.
+    },
+  },
 };
+
+platforms[platform].init();
