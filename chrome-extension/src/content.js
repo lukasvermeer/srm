2b250aad00555d5a7caf74f9b551770bdb7ef06b
Lukas Vermeer <lukas@lukasvermeer.nl>
2020-02-14 10:21:06 +0100

    Minor code style fixes by ESLint.

--- chrome-extension/src/content.js
+++ chrome-extension/src/content.js
@@ -15,7 +15,7 @@ const platform = window.location.host;
 
 // Checking for SRM using chi-square
 function checkSRM(observed, expected) {
-  let pval = computeSRM(observed, expected);
+  const pval = computeSRM(observed, expected);
   if (pval < params.pValueThreshold) {
     platforms[platform].flagSRM(pval);
   } else {
@@ -117,7 +117,7 @@ const platforms = {
                 sessioncounts.push(sessions);
                 weights.push(weight);
               }
-			  
+
               checkSRM(sessioncounts, weights);
               srmChecked = true;
             }
@@ -127,8 +127,8 @@ const platforms = {
     },
     flagSRM(pval) {
       document.querySelectorAll('.opt-variant-sessions-subtitle').forEach(i => i.style.cssText = 'background-color: red; color: white; padding: 1px 3px; border-radius: 3px;');
-	  document.querySelectorAll('.opt-variant-sessions-subtitle').forEach(i => i.title = 'SRM detected! p-value = ' + pval);
-	  document.querySelectorAll('.opt-variant-sessions-subtitle opt-variant-name').forEach(i => i.name = 'SRM detected! p-value = ' + pval);
+	  document.querySelectorAll('.opt-variant-sessions-subtitle').forEach(i => i.title = `SRM detected! p-value = ${pval}`);
+	  document.querySelectorAll('.opt-variant-sessions-subtitle opt-variant-name').forEach(i => i.name = `SRM detected! p-value = ${pval}`);
     },
     unflagSRM() {
       // TODO remove SRM warning if needed.
@@ -202,13 +202,13 @@ const platforms = {
     },
     flagSRM(pval) {
       document.querySelector('table.table--data tbody.ng-scope').querySelectorAll('tr.ng-scope strong.ng-binding').forEach(i => i.style.cssText = 'background-color: red; color: white; padding: 1px 3px; border-radius: 3px;');
-	  document.querySelector('table.table--data tbody.ng-scope').querySelectorAll('tr.ng-scope strong.ng-binding').forEach(i => i.title = 'SRM detected! p-value = ' + pval); // TODO - Check if working as expected
+	  document.querySelector('table.table--data tbody.ng-scope').querySelectorAll('tr.ng-scope strong.ng-binding').forEach(i => i.title = `SRM detected! p-value = ${pval}`); // TODO - Check if working as expected
     },
     unflagSRM() {
       // TODO remove SRM warning if needed.
     },
   },
-  
+
   // Convert.com
   'app.convert.com': {
     init() {
@@ -230,20 +230,20 @@ const platforms = {
           if (d) {
             const sessioncounts = [];
             const weights = [];
-			let groups = 0;
+            let groups = 0;
 
             // SESSIONS: Fill array
             for (let i = 0; i < d.length; i += 1) {
 			  if (d[i].style.display != 'none') {
-				groups++;
-				const sessions = parseInt(d[i].innerText.replace(/^[^/]+\/\s*([,0-9]+)/, '$1').replace(',',''));
-				sessioncounts.push(sessions);
+                groups++;
+                const sessions = parseInt(d[i].innerText.replace(/^[^/]+\/\s*([,0-9]+)/, '$1').replace(',', ''));
+                sessioncounts.push(sessions);
 			  }
             }
-			
+
             for (let i = 0; i < d.length; i += 1) {
               if (d[i].style.display != 'none') {
-				weights.push(1 / groups * 100);
+                weights.push(1 / groups * 100);
 			  }
             }
 
@@ -255,14 +255,14 @@ const platforms = {
       }, 1000);
     },
     flagSRM(pval) {
-      document.querySelector('div.summary-table-main.report_condensed').querySelectorAll('tr.tbody.vrow div.goal-data[data-rgroup=all]').forEach(i => i.style.cssText = i.style.cssText +';background-color: red; color: white; padding: 1px 3px; border-radius: 3px;');
-	  document.querySelector('div.summary-table-main.report_condensed').querySelectorAll('tr.tbody.vrow div.goal-data[data-rgroup=all]').forEach(i => i.title = 'SRM detected! p-value = ' + pval);
+      document.querySelector('div.summary-table-main.report_condensed').querySelectorAll('tr.tbody.vrow div.goal-data[data-rgroup=all]').forEach(i => i.style.cssText = `${i.style.cssText};background-color: red; color: white; padding: 1px 3px; border-radius: 3px;`);
+	  document.querySelector('div.summary-table-main.report_condensed').querySelectorAll('tr.tbody.vrow div.goal-data[data-rgroup=all]').forEach(i => i.title = `SRM detected! p-value = ${pval}`);
     },
     unflagSRM() {
       // TODO remove SRM warning if needed.
     },
   },
-  
+
   // Sitegainer.com
   'sitegainer.com': {
     init() {
@@ -287,27 +287,26 @@ const platforms = {
 
             // SESSIONS: Fill array
             for (let i = 0; i < d.length; i += 1) {
-			  const sessions = parseInt(d[i].querySelectorAll('td')[1].innerText.replace(/^\s*([,0-9]+)\s*\/.*?$/, '$1').replace(',',''));
+			  const sessions = parseInt(d[i].querySelectorAll('td')[1].innerText.replace(/^\s*([,0-9]+)\s*\/.*?$/, '$1').replace(',', ''));
 			  sessioncounts.push(sessions);
 			  const weight = parseInt(d[i].getAttribute('allocation'));
 			  if (weight != undefined && !isNaN(weight)) {
-				weights.push(weight);
+                weights.push(weight);
 			  }
             }
-			
+
             // Do SRM Check
-			if (Array.isArray(weights) && weights.length)
-			{
+            if (Array.isArray(weights) && weights.length) {
 			  checkSRM(sessioncounts, weights);
 			  srmChecked = true;
-			}
+            }
           }
         }
       }, 1000);
     },
     flagSRM(pval) {
-      document.querySelector('div.goalBlock').querySelectorAll('tr.resultRow').forEach(i => i.querySelectorAll('td')[1].style.cssText = i.style.cssText +';background-color: red; color: white; padding: 1px 3px; border-radius: 3px;');
-	  document.querySelector('div.goalBlock').querySelectorAll('tr.resultRow').forEach(i => i.querySelectorAll('td')[1].title = 'SRM detected! p-value = ' + pval);
+      document.querySelector('div.goalBlock').querySelectorAll('tr.resultRow').forEach(i => i.querySelectorAll('td')[1].style.cssText = `${i.style.cssText};background-color: red; color: white; padding: 1px 3px; border-radius: 3px;`);
+	  document.querySelector('div.goalBlock').querySelectorAll('tr.resultRow').forEach(i => i.querySelectorAll('td')[1].title = `SRM detected! p-value = ${pval}`);
     },
     unflagSRM() {
       // TODO remove SRM warning if needed.
