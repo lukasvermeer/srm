e57acffcac2dcc3d61e80cca8e6c2001d76ad54a
Lukas Vermeer <lukas@lukasvermeer.nl>
2020-03-05 14:51:13 +0100

    Exit survey after uninstall. Fixes #33.

--- chrome-extension/src/background.js
+++ chrome-extension/src/background.js
@@ -8,3 +8,11 @@ chrome.tabs.onUpdated.addListener(function
         })
     }
 });
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason == "install") {
+        if (chrome.runtime.setUninstallURL) {
+            chrome.runtime.setUninstallURL('https://forms.gle/Cgv34WVhgms9MChv7');
+        }
+    }
+});
