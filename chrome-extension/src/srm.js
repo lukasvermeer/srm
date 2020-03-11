lukasvermeer/srm

commit 934d367275fc221d39abf874e8cac5e282ec9ac7

Add a basic status indicator badge
First step towards providing users with more feedback about the SRM
check performed (e.g. #5 and/or #6).

I find it strange that I needed to add browser_action for this, but
there seems to be no way around this. I don't think this actually gives
the extension more permissions, but documentation is a bit unclear.

chrome-extension/src/srm.js
@@ -32,7 +32,9 @@ function checkSRM(observed, expected) {
   const pval = computeSRM(observed, expected);
   if (pval < params.pValueThreshold) {
     platforms[platform].flagSRM(pval);
+    chrome.runtime.sendMessage({badgeText: 'SRM', badgeBackgroundColor: 'red'});
   } else {
     platforms[platform].unflagSRM();
+    chrome.runtime.sendMessage({badgeText: 'OK', badgeBackgroundColor: 'green'});
   }
 }

chrome-extension/src/background.js
@@ -1,18 +1,43 @@
 // Send message to content.js when URL changes
 chrome.tabs.onUpdated.addListener(function
-    (tabId, changeInfo, tab) {
+  (tabId, changeInfo, tab) {
     if (changeInfo.url) {
-        chrome.tabs.sendMessage(tabId, {
-            message: 'URL has changed',
-            url: changeInfo.url
-        })
+      chrome.tabs.sendMessage(tabId, {
+        message: 'URL has changed',
+        url: changeInfo.url
+      })
     }
-});
+  });
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        if (chrome.runtime.setUninstallURL) {
-            chrome.runtime.setUninstallURL('https://forms.gle/Cgv34WVhgms9MChv7');
-        }
+  if (details.reason == "install") {
+    if (chrome.runtime.setUninstallURL) {
+      chrome.runtime.setUninstallURL('https://forms.gle/Cgv34WVhgms9MChv7');
     }
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log(message);
+  if (message.badgeBackgroundColor) {
+    chrome.browserAction.setBadgeBackgroundColor({color: message.badgeBackgroundColor});
+  }
+  if (message.badgeText) {
+    chrome.tabs.get(sender.tab.id, function(tab) {
+      if (chrome.runtime.lastError) {
+        return; // the prerendered tab has been nuked, happens in omnibox search
+      }
+      if (tab.index >= 0) { // tab is visible
+        chrome.browserAction.setBadgeText({tabId:tab.id, text:message.badgeText});
+      } else { // prerendered tab, invisible yet, happens quite rarely
+        var tabId = sender.tab.id, text = message.badgeText;
+        chrome.webNavigation.onCommitted.addListener(function update(details) {
+          if (details.tabId == tabId) {
+            chrome.browserAction.setBadgeText({tabId: tabId, text: text});
+            chrome.webNavigation.onCommitted.removeListener(update);
+          }
+        });
+      }
+    });
+  }
 });

chrome-extension/src/content.js
@@ -57,6 +57,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -145,6 +146,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -205,6 +207,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -278,6 +281,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -330,6 +334,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -407,6 +412,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -474,6 +480,7 @@ const platforms = {
       function newIframe() {
         if (location.href.slice(-8) === '/reports') {
           const oldIframe = document.getElementById('iframeforweight');
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           if (oldIframe != null) {
             oldIframe.parentNode.removeChild(oldIframe);
           }
@@ -493,6 +500,7 @@ const platforms = {
           if (request.message === 'URL has changed') {
             newIframe();
             srmChecked = false;
+            chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});
           }
         },
       );
@@ -555,3 +563,5 @@ const platforms = {
 };
 
 platforms[platform].init();
+
+chrome.runtime.sendMessage({badgeText: 'ON', badgeBackgroundColor: 'gray'});

commit ae9f42e6c1aaede8b5d35e4a2b90d97af7080496

Fix microsite by ensuring chrome.runtime is only used when present
Looks like I broke this in 934d367275fc221d39abf874e8cac5e282ec9ac7.
Oops.

chrome-extension/src/srm.js
@@ -32,9 +32,13 @@ function checkSRM(observed, expected) {
   const pval = computeSRM(observed, expected);
   if (pval < params.pValueThreshold) {
     platforms[platform].flagSRM(pval);
-    chrome.runtime.sendMessage({srmStatus: 'SRM'});
+    if (chrome.runtime) {
+      chrome.runtime.sendMessage({srmStatus: 'SRM'});
+    }
   } else {
     platforms[platform].unflagSRM();
-    chrome.runtime.sendMessage({srmStatus: 'OK'});
+    if (chrome.runtime) {
+      chrome.runtime.sendMessage({srmStatus: 'OK'});
+    }
   }
 }